3be9d7850cb9e0817b23c27870afc7ed3a64dc10
Daniel Maturana <dimatura@cmu.edu>
2015-11-29 17:18:03 -0500

    removed some old throwaway code

--- scripts/test_mmpack.py
+++
@@ -1,8 +0,0 @@
-
-import voxnet
-import msgpack
-
-with open('foo.msgpack', 'wb') as f:
-    for rec in voxnet.metrics_logging.read_records('foo.mlog'):
-        mrec = msgpack.packb(rec, use_bin_type=True)
-        f.write(mrec)
